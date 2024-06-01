649d73e022d73acc70a4ba6d0069d68d37d52f7f
Caleb Forestal | <133164966+Calebrlx@users.noreply.github.com>
2024-06-01 01:10:20 -0800

    fuck you

--- pages/api/og.jsx
+++ pages/api/og.jsx
@@ -8,59 +8,12 @@ export const config = {
 export default function handler() {
   return new ImageResponse(
     (
-      <div
-        style={{
-          display: 'flex',
-          height: '100%',
-          width: '100%',
-          alignItems: 'center',
-          justifyContent: 'center',
-          letterSpacing: '-.02em',
-          fontWeight: 700,
-          backgroundImage: 'linear-gradient(to bottom, #dbf4ff, #fff1f1)',
-        }}
-      >
-        <div
-          style={{
-            left: 42,
-            top: 42,
-            position: 'absolute',
-            display: 'flex',
-            alignItems: 'center',
-          }}
-        >
-          <span
-            style={{
-              width: 24,
-              height: 24,
-              background: 'black',
-              borderRadius: '50%',
-            }}
-          />
-          <span
-            style={{
-              marginLeft: 8,
-              fontSize: 20,
-            }}
-          >
-            Lawn Link Pro
-          </span>
+      <div style={{display:'flex',height:'100%',width:'100%',alignItems:'center',justifyContent:'center',letterSpacing:'-.02em',fontWeight:700,backgroundImage:'linear-gradient(to bottom, #dbf4ff, #fff1f1)'}}>
+        <div style={{left:42,top:42,position:'absolute',display:'flex',alignItems:'center'}}>
+          <span style={{width:24,height:24,background:'black',borderRadius:'50%'}} />
+          <span style={{marginLeft:8,fontSize:20}}>Lawn Link Pro</span>
         </div>
-        <div
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'center',
-            padding: '20px 50px',
-            margin: '0 42px',
-            fontSize: 40,
-            width: 'auto',
-            maxWidth: 550,
-            textAlign: 'center',
-            color: 'black',
-            lineHeight: 1.4,
-          }}
-        >
+        <div style={{display:'flex',flexWrap:'wrap',justifyContent:'center',padding:'20px 50px',margin:'0 42px',fontSize:40,width:'auto',maxWidth:550,textAlign:'center',color:'black',lineHeight:1.4}}>
           <span>Simplifying Your Lawn Care Business Online Presence</span>
         </div>
       </div>
