b8980b8b166f47614efa2efc0912d8317479e175
Caleb Forestal | <133164966+Calebrlx@users.noreply.github.com>
2024-06-01 01:26:48 -0800

    fuck you, vercel

---
+++ pages/api/og.jsx
@@ -0,0 +1,26 @@
+// /* eslint-disable @next/next/no-img-element */
+// import { ImageResponse } from '@vercel/og';
+
+// export const config = {
+//   runtime: 'edge',
+// };
+
+// export default function handler() {
+//   return new ImageResponse(
+//     (
+//       <div style={{display:'flex',height:'100%',width:'100%',alignItems:'center',justifyContent:'center',letterSpacing:'-.02em',fontWeight:700,backgroundImage:'linear-gradient(to bottom, #dbf4ff, #fff1f1)'}}>
+//         <div style={{left:42,top:42,position:'absolute',display:'flex',alignItems:'center'}}>
+//           <span style={{width:24,height:24,background:'black',borderRadius:'50%'}} />
+//           <span style={{marginLeft:8,fontSize:20}}>Lawn Link Pro</span>
+//         </div>
+//         <div style={{display:'flex',flexWrap:'wrap',justifyContent:'center',padding:'20px 50px',margin:'0 42px',fontSize:40,width:'auto',maxWidth:550,textAlign:'center',color:'black',lineHeight:1.4}}>
+//           <span>Simplifying Your Lawn Care Business Online Presence</span>
+//         </div>
+//       </div>
+//     ),
+//     {
+//       width: 1200,
+//       height: 630,
+//     }
+//   );
+// }
