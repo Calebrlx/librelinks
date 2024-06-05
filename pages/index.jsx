975c89b7fb3f28b80889d3e7e84774a0e64ffaa6
Caleb Forestal | <133164966+Calebrlx@users.noreply.github.com>
2024-06-05 00:52:34 -0800

    k

--- pages/index.jsx
+++ pages/index.jsx
@@ -121,15 +121,6 @@ const Home = () => {
                   <h3 className="lg:block">LawnLinkPro</h3>
                 </Link>
                 <div className="relative items-center w-28 z-10 md:absolute md:inset-y-0 md:right-0">
-                  <a
-                    className="group inline-flex items-center gap-2 px-4 text-sm bg-slate-900 border rounded-3xl text-white w-[116px] h-[35px] justify-center font-semibold transition-colors hover:bg-slate-700"
-                    rel="noopener noreferrer"
-                    href="mailto:caleb@alyeska.dev"
-                  >
-                    Contact Sales
-                  </a>
-                </div>
-                {/* <div className="relative items-center w-28 z-10 md:absolute md:inset-y-0 md:right-0">
                   <Link
                     className="group inline-flex items-center gap-2 px-4 text-sm  bg-slate-900 border rounded-3xl text-white w-[116px] h-[35px] justify-center font-semibold transition-colors hover:bg-slate-700"
                     rel="noopener noreferrer"
@@ -137,7 +128,7 @@ const Home = () => {
                   >
                     {isAuthenticated ? 'Admin' : 'Login'}
                   </Link>
-                </div> */}
+                </div>
               </nav>
             </div>
             <div className="px-4 mx-auto mt-24 max-w-7xl sm:mt-16 sm:px-6">
